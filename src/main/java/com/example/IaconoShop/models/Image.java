a12253e66809217cb6bb700ec94ed43634bc86b5
Gabriele-Iacono <gabry20035@yahoo.it>
2024-10-21 14:20:56 +0200

    feat: new models to manage products with categories

--- src/main/java/com/example/IaconoShop/models/Image.java
+++ src/main/java/com/example/IaconoShop/models/Image.java
@@ -6,18 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+import java.sql.Blob;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Category {
+
+public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String fileName;
+    private String fileType;
 
-    @OneToMany(mappedBy = "category")
-    private List<Product> products;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
 }
